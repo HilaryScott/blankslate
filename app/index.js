4a6ced5bb33bfd912701e5ffe5069b8df8994d8d
Hilary Scott <hscott721@gmail.com>
2017-04-19 15:22:32 -0400

    React Setup

---
+++ app/index.js
@@ -0,0 +1,9 @@
+/* ./app/index.js - Webpack entry file */
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home.jsx';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+
